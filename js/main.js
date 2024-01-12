eb1a5d73a4e2e46beb3e3cccb83c5562638a455a
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2024-01-12 02:50:34 +0100

    funcion paginacion terminada

--- js/main.js
+++ js/main.js
@@ -29,7 +29,10 @@ const arrayFiltro = [{
 }];
 
 const perPage = 12;
+
 let palabraValida;
+let paginaAnterior;
+let siguientePagina;
 
 //Eventos: Change, Click, Submit
 
@@ -37,9 +40,10 @@ formularioBuscar.addEventListener("submit", (ev) => {
     ev.preventDefault()
     const tag = buscar.value
     validar(tag)
-    console.log(tag)
+    //console.log(tag)
     const url = `search?query=${palabraValida}&per_page=${perPage}`
     pintarGaleria(url)
+    pintarPaginacion()
     
     
 })
@@ -51,16 +55,25 @@ document.addEventListener("click", (ev) => {
         //console.log(tag)
         const url = `search?query=${tag}&per_page=${perPage}`
         pintarGaleria(url)
+        pintarPaginacion()
     }
+    if (ev.target.matches("#botonesPaginacion #nextPage")){
+        const urlpag=siguientePagina.replace(urlBase,'')
+        pintarGaleria(urlpag)
+        }
+    
+        if (ev.target.matches("#botonesPaginacion #prevPage")){
+            const urlpag=paginaAnterior.replace(urlBase,'')
+            pintarGaleria(urlpag)
+            }
 
 })
 
 selectPosicionImg.addEventListener("change", (ev) => {
     const valor = ev.target.value
     
-    console.log(valor)
+    console.log("la orientacion es:",valor)
 
-    //console.log(valor)
     
     
 })
@@ -109,8 +122,8 @@ const validar = (tag) => {
         error.append(mensaje)
     } else{
         palabraValida = tag
-        console.log("en validado")
-        console.log(palabraValida)
+        console.log("validado")
+        //console.log(palabraValida)
     }
 }
 
@@ -174,8 +187,9 @@ const pintarGaleria = async (url) => {
         const foto=await respuesta.respuesta
         
         //console.log(foto)
-       const {photos} = foto
-
+        const { photos,next_page,prev_page } = foto
+        nextPagina(next_page)
+        prevPagina(prev_page)
        photos.forEach((item)=>{
             //console.log(item)
             const caja = document.createElement('ARTICLE')
@@ -202,9 +216,28 @@ const pintarGaleria = async (url) => {
    
 }
 
-const pintarPaginacion = (url) => {
+const nextPagina=(next_page)=>{  
+    siguientePagina=next_page
+   // console.log("en funcion nextPagina",next_page)   
+}
+const prevPagina=(prev_page)=>{  
+    paginaAnterior=prev_page
+  //  console.log("en funcion prevPagina",prev_page)   
+}
+const pintarPaginacion = () => {
 
+botonesPaginacion.innerHTML='';
+const caja=document.createElement("DIV")
+const botonPrev=document.createElement("BUTTON")
+botonPrev.id="prevPage"
+botonPrev.textContent="<Prev pagina"
+const botonNext=document.createElement("BUTTON")
+botonNext.id="nextPage"
+botonNext.textContent="Next pagina>"
 
+caja.append(botonPrev,botonNext)
+fragment.append(caja)
+botonesPaginacion.append(fragment)
 
 }
 
